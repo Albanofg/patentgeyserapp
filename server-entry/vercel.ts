c1d0af8c3c586d2ffc492c1a91fc7df10dcfba9c
Albano Flavio Gonzalez Cartier <gonzalezalbanof@gmail.com>
2026-04-14 13:42:57 -0300

    switch

--- server-entry/vercel.ts
+++ server-entry/vercel.ts
@@ -1,4 +1,4 @@
-import { app, ensureInitialized } from "../server/app.js";
+import { app, ensureInitialized } from "../server/app";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
